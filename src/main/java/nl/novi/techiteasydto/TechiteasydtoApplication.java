76e58335ebca8e017efce2d9d7ab81c9e04fd84f
Akatsuki Rinnegan <shea.farmer@novi-education.nl>
2024-02-26 23:23:31 +0100

    first commit

---
+++ src/main/java/nl/novi/techiteasydto/TechiteasydtoApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasydto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechiteasydtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechiteasydtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/techiteasydto/TechiteasydtoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.techiteasydto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechiteasydtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
